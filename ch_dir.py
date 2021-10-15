64ad52ab00c622c68aec42057a54df89f48aef23
Adam <adam.rocq@gmail.com>
2021-10-14 22:44:35 -0400

    Added logging AND working now!

---
+++ ch_dir.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import urllib.request
+
+
+
+dir = os.getcwd()
+print(dir)
+
+os.chdir('../')
+dir = os.getcwd()
+print(dir)
+
+sys.path
+sys.path.append('E:\Repos\Spotify_DL')
+
+print(sys.path)
--- test.py
+++ test.py
@@ -1,8 +1,36 @@
 import json
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
 import re
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import sys
 import urllib.request
+
+sys.path
+sys.path.append('E:\Repos\Spotify_DL')
+
+FAIL_DICT = {}
+
+PLAYLIST_LENGTH = 0
+
+# Change working directory so logs can hit the logging folder
+os.chdir('../')
+
+# Check if the logging directory exists, create if not
+if not os.path.isdir('Logging/spotify_downloader/'):
+    os.mkdir('Logging/spotify_downloader/')
+
+logger = logging.getLogger('[Python Spotify Downloader]')
+logger.setLevel(logging.DEBUG)
+logname = 'Logging/spotify_downloader/spotify_downloader.log'
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler = logging.FileHandler(logname)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 
 def get_playlist_song_artist(playlist_raw):
     get_playlist_list = []
@@ -15,64 +43,65 @@ def get_playlist_song_artist(playlist_raw):
         for artist in song['track']['artists']:
             detail_list.append(get_youtube_search_join(artist['name']))
         get_playlist_list.append(detail_list)
+        logger.info("Adding the following song and artist to our list: \n {0}".format(detail_list))
 
     return get_playlist_list
 
 
 def get_youtube_search_join(raw_search_string):
+    logger.info("Processing this raw search string, into a suffix for a YouTube search: {0}".format(raw_search_string))
     search_string_split = raw_search_string.split(" ")
     search_string_joined = "+".join(search_string_split)
 
+    logger.info("Processed string {0}".format(search_string_joined))
     return search_string_joined
 
 
 def get_youtube_url(search_list):
-    searh_params = "+".join(search_list)
-    search_results = urllib.request.urlopen("https://www.youtube.com/results?search_query={0}".format(searh_params))
+    logger.info("Received this search string: ".format(search_list))
+    search_params = "+".join(search_list)
 
+    logger.info("Searching with the following link: "
+                "\nhttps://www.youtube.com/results?search_query={0}".format(search_params))
+    search_results = urllib.request.urlopen("https://www.youtube.com/results?search_query={0}".format(search_params))
+
+    # Using RegEx to search for all video results that have an 11 AlphaNumeric code, revealing the video URL suffix
     video_ids = re.findall(r"watch\?v=(\S{11})", search_results.read().decode())
-    youtube_link = "https://www.youtube.com/watch?v=" + video_ids[0]
+    video_ids[0]
 
-    return youtube_link
+    return video_ids, search_params
 
 
 playlist_url = 'https://open.spotify.com/playlist/51gY6jFMaqpsPFFgVbGcvX?si=19584ccb85d24650'
 playlist_id = playlist_url.split('/')[-1]
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-# results = spotify.playlist_items(playlist_id)['tracks']['items'][0]['track']
 results = spotify.playlist_items(playlist_id)
 
 playlist_results = get_playlist_song_artist(results)
 
 print(json.dumps(playlist_results, indent=4, sort_keys=True))
 
-for track in playlist_results:
-    print(get_youtube_url(track))
-# print(playlist_dict)
+for track_count, track in enumerate(playlist_results):
+    try:
+        # print(get_youtube_url(track))
+        video_ids_get, search_params_get = get_youtube_url(track)
+        for vid_trial_count, video in enumerate(video_ids_get):
+            # video_suffix = get_youtube_url(track)
+            url = "https://www.youtube.com/watch?v=" + video
 
-# print(json.dumps(playlist_dict, indent=4, sort_keys=True))
-# song_title = results['name']
+            dest_path = '/Downloaded_Music'
+            yt_dl_cmd = 'youtube-dl -v -f bestaudio {0} ' \
+                        '--external-downloader ffmpeg --external-downloader-args "-ss starttime -to endtime" -o ' \
+                        '"{1}/%(title)s.%(ext)s"'.format(url, dest_path)
+            try:
+                print("Downloading song {0} of {1}".format(track_count+1, len(playlist_results)))
+                logger.info("Downloading song {0} of {1}".format(track_count+1, len(playlist_results)))
+                os.system(yt_dl_cmd)
+                break
+            except Exception as e:
+                logger.exception("Encountered an issue: {0}".format(e))
+                FAIL_DICT[search_params_get[vid_trial_count]] = e
 
-# artists_list = results['artists']
-# print(artists_list)
-# for artist in artists_list:
-#     print(artist['name'])
-# albums = results['items']
-# while results['next']:
-#     results = spotify.next(results)
-#     albums.extend(results['items'])
-#
-# for album in albums:
-#     print(album['name'])
-
-
-# url = 'https://www.youtube.com/watch?v=5T0utQ-XWGY'
-# dest_path = '/mnt/c/Users/adamr/My Documents/My Music/Downloaded_Music/Ready_To_Stumble'
-# yt_dl_cmd = 'youtube-dl -v -f bestaudio {0} ' \
-#             '--external-downloader ffmpeg --external-downloader-args "-ss starttime -to endtime" -o ' \
-#             '"{1}/%(title)s-%(id)s.%(ext)s"'.format(url, dest_path)
-#
-# print(yt_dl_cmd)
-#
-# os.system(yt_dl_cmd)
+    except Exception as e:
+        logger.exception("Encountered an issue: {0}".format(e))
